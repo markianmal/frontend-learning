8d536f36d61dd8ee95e3b4c29757ee0bed0703fd
marki1n-code <mramur47@gmail.com>
2026-01-29 01:57:40 +0200

    fixed id ><

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-console.log(`Hello JS!`);
-console.log(`Hello JS!`);
-console.log(`Hello JS!`);
+//console.log("Hello JS!");
+
+//window.alert("Shit");
+
+document.getElementById("myH1").textContent = "HTML CSS JS";
+document.getElementById("myP").textContent = "You need to love yourself";

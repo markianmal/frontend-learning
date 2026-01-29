bf612e08e5f3b5ab337ea7ee97a3bd7c64a0d4ac
marki1n-code <mramur47@gmail.com>
2026-01-29 22:03:49 +0200

    Refactor project structure: move HTML, CSS, and JS files to js01.hellojs directory and update content

--- index.js
+++
@@ -1,6 +0,0 @@
-//console.log("Hello JS!");
-
-//window.alert("Shit");
-
-document.getElementById("myH1").textContent = "HTML CSS JS";
-document.getElementById("myP").textContent = "You need to love yourself";
---
+++ js01.hellojs/script.js
@@ -0,0 +1,13 @@
+//console.log("Hello JS!");
+
+//window.alert("Shit");
+
+document.getElementById("myH1").textContent = "HTML CSS JS";
+document.getElementById("myP").textContent = "You need to love yourself";
+document.getElementById("myP1").textContent = "Не біжи за метеликами, зроби гарний сад і метелики самі прийдуть, а якщо не прийдуть ти все одно матимеш гарний сад.  ";
+document.getElementById("myP2").textContent = "80/20 rule";
+
+
+let name = "Markian";
+
+console.log(name);

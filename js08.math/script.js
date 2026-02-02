52097dd1a2453955d0c971062a29659e67e87144
marki1n-code <mramur47@gmail.com>
2026-02-02 23:48:06 +0200

    math

---
+++ js08.math/script.js
@@ -0,0 +1,29 @@
+//console.log(Math.PI);
+//console.log(Math.E);
+
+let x = 5.5;
+let y = 2.5;
+let z;
+
+z = Math.round(x);
+
+/*
+z = Math.floor(x); // rounds for < smaller num
+z = Math.ceil(x);  // rounds for > bigger  num
+z = Math.trunc(x);  // no 1.3... just x 1
+z = Math.pow(x);   //
+z = Math.sqrt(x);  //
+z = Math.log(x);   //
+z = Math.sin(x);   //
+z = Math.cos(x);   //
+z = Math.tan(x);   //
+z = Math.abs(x);   // -4.4 = 4.4 []
+z = Math.sign(x);  // -6 = -1 // 6 = 1 // 0 = 0
+*/
+
+let max = Math.max(x, y, z);
+let min = Math.min(x, y, z);
+
+console.log(z);
+console.log(max);
+console.log(min);

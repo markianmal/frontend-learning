810542373975acecf7dfb6fe871172b26b702c9b
marki1n-code <mramur47@gmail.com>
2026-02-04 16:25:37 +0200

    practiced js payment options

---
+++ js12.ternary-operator/script.js
@@ -0,0 +1,15 @@
+// ternary operator = a shortcut to if{} and else{} statements
+//                                  helps to assign a variable based on a condition
+//                                  condition ? codeIfTrue : codeIfFalse;
+
+//let time = 9;
+//let greeting = time < 12 ? "Good morning!" : "Good afternoon!";
+//console.log(greeting);
+
+//let isStudent = false;
+//let message = isStudent ? "You are a student" : "You are NOT a student";
+//console.log(message);
+
+let purchaseAmount = 125;
+let discount = purchaseAmount >= 100 ? 10 : 0;   //discount 10 or 0
+console.log(`Your total is $${purchaseAmount - purchaseAmount * (discount / 100)}`);
---
+++ js13.switches/script.js
@@ -0,0 +1,33 @@
+let day = 1;
+
+switch (day) {
+    case 1:
+        console.log("it is mo");
+        break;
+
+    case 2:
+
+        console.log("it is tu");
+        break;
+
+    case 3:
+        console.log("it is we");
+        break;
+
+    case 4:
+        console.log("it is th");
+        break;
+
+    case 5:
+        console.log("it is fr");
+        break;
+    case 6:
+        console.log("it is se");
+        break;
+    case 7:
+        console.log("it is su");
+        break;
+
+    default:
+        break;
+}

e4df7c23c69a30af9151116a765ce59b5b1da766
marki1n-code <mramur47@gmail.com>
2026-02-07 21:36:39 +0200

    14

---
+++ js14.strings-methods/script.js
@@ -0,0 +1,13 @@
+let userName = "Markian ";
+console.log(userName.charAt(0));
+
+userName.indexOf("M");
+console.log(userName.indexOf("n")); //n = 6
+
+console.log(userName.length);//length of a string is 8 chare...
+
+userName = userName.trim();// removes " "
+console.log(userName);
+
+userName = userName.toUpperCase();
+console.log(userName);

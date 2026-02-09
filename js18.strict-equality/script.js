a7688402b91b247792474549939a10ca82e282ab
marki1n-code <mramur47@gmail.com>
2026-02-09 19:00:30 +0200

    = == === != !==

---
+++ js18.strict-equality/script.js
@@ -0,0 +1,20 @@
+//   = for variables
+//  == comparison
+// === strict equality
+// !=  f
+// !== f 
+const PI = 3.14;
+
+/*if (PI === "3.14") { //becouse num 3.14 isnt string "3.14"
+    console.log("Thats is PI");
+}
+else {
+    console.log("That is not PI")
+}*/
+
+if (PI != "3.14") { //becouse num 3.14 isnt string "3.14"
+    console.log("Thats is not PI");
+}
+else {
+    console.log("That is PI")
+}

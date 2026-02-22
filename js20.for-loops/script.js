3e3748ad9ac92cb0a43421a20e70e39a128d7a9e
marki1n-code <mramur47@gmail.com>
2026-02-22 17:27:59 +0200

    for loops

--- js20.for-loops/script.js
+++ js20.for-loops/script.js
@@ -1,6 +1,27 @@
-for (let i = 10; i >= 1; i--) {
+//repeating code limited amount of times
+
+for (let i = 20; i >= 1; i--) {
+
+    if (i == 14) {
+        continue;//важлтво щоб воно було тут
+    }
 
     console.log(i);
+
+    if (i == 13) {
+        console.log("Lucky thirteen");
+    }
+
+    if (i == 15) {
+        console.log("Skips num 14");
+    }
+
+
+
+
+    if (i == 6) {
+        break;
+    }
 }
 
 console.log("Heppy new year");

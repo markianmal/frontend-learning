29b51483afc9f66dd0be8d884e7e2bac77a5119b
marki1n-code <mramur47@gmail.com>
2026-02-27 23:47:26 +0200

    func

---
+++ js22.func/script.js
@@ -0,0 +1,11 @@
+//function = a section of reusable code
+
+function HappyBirthday() {
+
+    console.log("Happy birthday to you!");
+    console.log("Happy birthday to you!");
+    console.log("Happy birthday dear you!");
+    console.log("Happy birthday to you!");
+}
+
+HappyBirthday();

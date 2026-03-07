f031ed48038b8dd7585709a3fbea86365d6d033c
marki1n-code <mramur47@gmail.com>
2026-03-07 14:30:55 +0200

    func

--- js22.func/script.js
+++ js22.func/script.js
@@ -1,6 +1,6 @@
 //function = a section of reusable code
 
-function HappyBirthday(username, age) {
+/*function HappyBirthday(username, age) {
 
     console.log("Happy birthday to you!");
     console.log("Happy birthday to you!");
@@ -10,3 +10,19 @@ function HappyBirthday(username, age) {
 }
 
 HappyBirthday("Markian", 18);
+*/
+
+function add(x, y) {
+    return x + y;
+}
+function substruct(x, y) {
+    return x - y;
+}
+function multiply(x, y) {
+    return x * y;
+}
+function devide(x, y) {
+    return x / y;
+}
+
+console.log(devide(2, 3));

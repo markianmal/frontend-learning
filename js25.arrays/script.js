5b66f617f0757ad1513fd0dcd58f9f7a8d588731
marki1n-code <mramur47@gmail.com>
2026-03-26 22:53:14 +0200

    arrays

--- js25.arrays/script.js
+++ js25.arrays/script.js
@@ -14,3 +14,17 @@ function cube(element) {
     return Math.pow(element, 3)
 }
 
+const students = ["Markian", "Nazar", "Max", "Pavlo", "Volodia"];
+const studentsUpper = students.map(upperCase);
+const studentsLower = students.map(LowerCase);
+
+console.log(studentsUpper);
+console.log(studentsLower); 
+
+function upperCase(element) {
+    return element.toUpperCase();
+}
+
+function LowerCase(element) {
+    return element.toLowerCase();
+}

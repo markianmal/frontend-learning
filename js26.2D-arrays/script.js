962afb629bdfe2860947485c3e8b42803bfec37c
marki1n-code <mramur47@gmail.com>
2026-04-08 23:36:21 +0300

    ...spread operator

---
+++ js26.2D-arrays/script.js
@@ -0,0 +1,9 @@
+let username = "Markian Maletskyi";
+let letters = [...username].join("-");
+
+console.log(letters);
+
+let vegetables = ["carrots" ,"potatoes" ,"tomatos"];
+let all = [...vegetables, username]
+
+console.log(all);
